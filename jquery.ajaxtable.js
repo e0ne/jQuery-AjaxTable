4c16665bf3b20fd8206acd53f5a9a98045a3ffdb
Ivan Kolodyazhny <e0ne@e0ne.info>
2011-08-26 02:04:03 +0300

    Fix sort feature

--- jquery.ajaxtable.js
+++ jquery.ajaxtable.js
@@ -14,16 +14,18 @@
  *    License for the specific language governing permissions and limitations
  *    under the License.
  */
-
 (function( $ ){
     var methods = {
         init : function( options ) {
             var settings = {
                 'template' : 'ajax_table_template',
                 'url' : '',
+                'autoUpdate' : true,
                 'interval' : 5000,
                 'beforeUpdate' : null,
-                'afterUpdate': null
+                'afterUpdate' : null,
+                'sortable': false,
+                'cacheable': false // NOTE: this param isn't used in current version
             };
             if ( options ) {
                 $.extend( settings, options );
@@ -36,35 +38,130 @@
                 $(this).data('ajaxTable', {
                     timers : {},
                     beforeUpdate : settings.beforeUpdate,
-                    afterUpdate : settings.afterUpdate
+                    afterUpdate : settings.afterUpdate,
+                    cachedData: null
                 });
                 data = $this.data('ajaxTable');
-                
+
             }
-            
+           
             var selector = this;
+
+            var findRow = function(dataItems, dataValue, column){
+                for (var i=0; i<dataItems.length; i++){
+                    var dataItem = dataItems[i];
+                    for (var p in dataItem){
+                        if (p == column && dataItem[p] == dataValue){
+                            return dataItem;
+                        }
+                    }
+                }
+                return null;
+            }
+
+            var sort = function(sortOrder, column){
+                var sorted_data = [];
+                if (data.cachedData){
+                    var sorted_column = new Array();
+                    for (var i=0;i<data.cachedData.length; i++)
+                    {
+                        var currentItem = data.cachedData[i];
+                        var dataItem = null;
+                        for (var pi in currentItem)
+                        {
+                            if (pi == column){
+                                dataItem = currentItem[pi];
+                            }
+                        }
+                        sorted_column.push(dataItem);
+                    }
+                    sorted_column = sorted_column.sort();
+
+                    if (sortOrder == 'desc') {
+                        sorted_column = sorted_column.reverse();
+                    }
+
+                    for (i=0; i< sorted_column.length; i++){
+                        // TODO: fix issue with non unique data in columns
+                        // need to check all fields
+                        var line = findRow(data.cachedData, sorted_column[i], column)
+                        sorted_data.push(line);
+                    }
+                }
+                selector.each(function(){
+                    var elem = this;
+                    var body = $(elem).children("tbody");
+                    body.html("");
+                    $("#" + settings.template).tmpl(sorted_data).appendTo(body);
+                });
+            }
+
+            var initSort = function(isUpdated, tableData){
+                var cache = new Array();
+                selector.each(function(){
+                    $(this).children("thead").children("tr").children("th").click(function(){
+                        var th = $(this);
+                        var sortOrder = th.attr("sortOrder");
+
+                        if (sortOrder){
+                            if (sortOrder == 'asc'){
+                                sortOrder = 'desc';
+                            } else {
+                                sortOrder = 'asc';
+                            }
+                        } else {
+                            sortOrder = 'asc';
+                        }
+                        var tableHeaders = selector.children("thead").children("tr").children("th");
+                        tableHeaders.removeAttr("sortOrder");
+                        tableHeaders.removeClass("sorted-asc");
+                        tableHeaders.removeClass("sorted-desc");
+                        th.attr("sortOrder", sortOrder);
+                        th.addClass("sorted-"+sortOrder);
+                        sort(sortOrder, th.attr("key"));
+                    });
+                });
+                data.cachedData = tableData;
+            }
+
             function update(){
+                selector.children("thead").children("tr").children("th").unbind("click");
+                
                 if (data.beforeUpdate){
                     data.beforeUpdate();
                 }
 
                 selector.each(function(){
                     var elem = this;
-                    $.get(settings.url, function(data){
+                    $.get(settings.url, function(response){
                         var body = $(elem).children("tbody");
                         body.html("");
-                        $("#" + settings.template).tmpl(data).appendTo(body);
+                        $("#" + settings.template).tmpl(response).appendTo(body);
+
+                        if (data.afterUpdate){
+                            data.afterUpdate();
+                        }
+                        if (settings.sortable){
+                            var isUpdated = true;
+                            if (settings.cacheable){
+                                isUpdated = false;
+                            }
+                            initSort(isUpdated, response);
+                            selector.children("thead").children("tr").children("th.sorted-asc").each(function(){
+
+                                sort($(this).attr("sortOrder"), $(this).attr("key"));
+                            });
+                            selector.children("thead").children("tr").children("th.sorted-desc").each(function(){
+                                sort($(this).attr("sortOrder"), $(this).attr("key"));
+                            });
+                        }
                     });
                 });
-
-                if (data.afterUpdate){
-                    data.afterUpdate();
-                }
             }
 
             update();
 
-            if (!data.timers[this.selector]){
+            if (settings.autoUpdate && !data.timers[this.selector]){
                 data.timers[this.selector] = window.setInterval(update, settings.interval);
             }
         },
