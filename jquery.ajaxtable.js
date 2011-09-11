16c2e869e59e96d8627c4e4752510a93a4c8f990
Ivan Kolodyazhny <e0ne@e0ne.info>
2011-09-11 15:34:37 +0300

    Some bugfixes

--- jquery.ajaxtable.js
+++ jquery.ajaxtable.js
@@ -176,6 +176,8 @@
                             selector.children("thead").children("tr").children("th.sorted-desc").each(function(){
                                 sort($(this).attr("sortOrder"), $(this).attr("key"));
                             });
+
+                        setTimeout(3000, update)
                         }
                     });
                 });
